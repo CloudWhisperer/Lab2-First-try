92ead1d9f97388590bf16ae8920faf20ed286c8f
Your Name <you@example.com>
2020-10-05 16:56:11 +0100

    Yay new artowkr

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ void on_cleanup()
 
 int main(int argc, char* args[])
 {
-	util::sdl_initialize("Change 22", 800, 600);
+	util::sdl_initialize("Change art", 800, 600);
 
 	on_load();
 	SDL_SetWindowIcon(util::sdl_window, icon);
